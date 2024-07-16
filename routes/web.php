f7a930c56a77fbd6ffb6e410e0102bdb94221bef
Juan Cigales <jcigales@192.168.1.121>
2024-07-16 12:49:48 +0200

    Auth

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,18 @@
 <?php
 
+use App\Http\Controllers\ResumeController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::resource('resumes', ResumeController::class);
 
 Auth::routes();
 
